13ee20851d8f0c08283ee597fd2a62bd743bd0a9
Charlie Carter <charliec1665@gmail.com>
2022-09-28 16:23:43 -0400

    added db.query to server.js

--- server.js
+++ server.js
@@ -10,15 +10,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Connect to MySQL database
-// const db = mysql.createConnection(
-//     {
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'Faemoon89',
-//         database: 'election'
-//     },
-//     console.log('Connected to the election database.')
-// );
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Faemoon89',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
