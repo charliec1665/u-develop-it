7d73896f7779eaa635dc060abdd82cbbcafe88d1
Charlie Carter <charliec1665@gmail.com>
2022-09-28 16:37:45 -0400

    constructed query statements

--- server.js
+++ server.js
@@ -20,9 +20,40 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// DELETE a candidate
+// Here DELETE statement has a ? that denotes a placeholder, making this a *prepared statement*
+    // a prepared statement can execute the same SQL statements repeatedly using different values in place of the ?
+    // the additional param argument following the prepared statement (here it is 1) provides values to use in place of the ?
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//     VALUES (?, ?, ?, ?)`;
+// const params = [1, 'Ronald', 'Fairbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
